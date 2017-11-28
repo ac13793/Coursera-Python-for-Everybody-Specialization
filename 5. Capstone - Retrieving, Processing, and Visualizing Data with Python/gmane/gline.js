581474e43da061d5d37e525a5f77d6076108d3a6
Ankit Chaurasia <ac13793@gmail.com>
2017-11-28 19:06:44 +0530

    Capstone Week4 Assignment

---
+++ 5. Capstone - Retrieving, Processing, and Visualizing Data with Python/gmane/gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Year','cam.ac.uk','umich.edu','rsmart.com','mtu.edu','virginia.edu','unicon.net','northwestern.edu','mac.com','suny.edu','columbia.edu'],
+['2005-12',3,2,2,2,2,2,2,1,1,1]
+];
